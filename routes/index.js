dd03966b2db3ba07add881ac7ac32fada64611d5
Kristopher Miles <kristophermiles@gmail.com>
2017-11-16 00:31:07 +0000

    final revision for now

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var schedule = require ('node-schedule');
+var emailer = require ('nodemailer');
+var Email = mongoose.model('Email');
+
+router.get('/messages', function(req, res, next) {
+  Email.find(function(err, messages){
+    if(err){ return next(err); }
+    res.json(messages);
+  });
+});
+
+router.post('/messages', function(req, res, next) {
+  var email = new Email(req.body);
+  email.save(function(err, comment){
+    if(err){ return next(err); }
+   //This is where the magic happens.
+    res.json(email);
+  });
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
